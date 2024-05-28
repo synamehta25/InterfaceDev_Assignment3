7cc5dc884a4568478650eb17d4d6c076c826a7f0
jesstashia <gueong26396@gmail.com>
2024-05-28 20:19:08 +1000

    finishing product details page

---
+++ javascript/javascriptfile.js
@@ -0,0 +1,25 @@
+
+/* js for product gallery*/
+{/* <script>
+    var Productimg = document.getElementById("Productimg");
+    var Smallimg = document.getElementsByClassName("small-img")
+
+    SmallImg[0].onclick = function()
+    {
+        ProductImg.src = SmallImg[0].src;
+    }
+    SmallImg[1].onclick = function()
+    {
+        ProductImg.src = SmallImg[0].src;
+    }
+    SmallImg[2].onclick = function()
+    {
+        ProductImg.src = SmallImg[0].src;
+    }
+    SmallImg[3].onclick = function()
+    {
+        ProductImg.src = SmallImg[0].src;
+    }
+</script>
+</body>
+</html> */}
